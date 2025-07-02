ea6b085cad28a58da16dddb849b8f0457b54ec2c
tj003 <tj0032@naver.com>
2025-07-02 13:16:34 +0900

    0702

--- src/operation/op4.java
+++ src/operation/op4.java
@@ -5,7 +5,35 @@ public class op4 {
         int a = 10;
         int b = 20;
         int c = 30;
+
+
         // 3수의 최대 값과 최소 값을 구하시오
+        int max = a;
+        if (b > max) max = b;
+        if (c > max) max = c;
+
+        int min = a;
+        if (b < min) min = b;
+        if (c < min) min = c;
+
+        System.out.println("최대값: " + max);
+        System.out.println("최소값: " + min);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ src/operation/op5.java
@@ -0,0 +1,16 @@
+package operation;
+
+public class op5 {public static void main(String[] args) {
+    int a = 7;
+    int b = 3;
+    double c = a * 1.0 / b;
+    System.out.printf("%3.3F \n",c);
+    double d1 = 3.54563;
+    double d2 = 2.56423;
+    int i = (int)d1 + (int)d2;
+    System.out.println(i);
+
+
+
+    }
+}
