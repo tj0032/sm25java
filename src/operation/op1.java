0b0cf3609e6944f083ce11b27f7971fbd8f0dff9
tj003 <tj0032@naver.com>
2025-07-02 11:08:50 +0900

    0702

---
+++ src/operation/op1.java
@@ -0,0 +1,14 @@
+package operation;
+
+public class op1 {
+    public static void main(String[] args){
+        int num1 = 10;
+        int num2 = 20;
+        int result = num1++ + ++num2;
+        System.out.printf("%d %d %d", num1, num2, result);
+
+
+
+
+    }
+}
