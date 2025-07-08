14f4b1f216260ad72df112b1a6da90d24aa96633
tj003 <tj0032@naver.com>
2025-07-08 15:41:20 +0900

    0708

--- jdbc02/src/test/java/cust/Delete.java
+++ jdbc02/src/test/java/cust/Delete.java
@@ -1,4 +1,21 @@
 package cust;
 
+import eud.sm.dto.Cust;
+import eud.sm.service.CustService;
+import org.junit.jupiter.api.Test;
+
 public class Delete {
+
+    @Test
+    public void insert(){
+        System.out.println("delete Test Start ...");
+        CustService custService = new CustService();
+        try {
+            custService.remove("id56");
+            System.out.println("삭제 정상");
+        } catch (Exception e) {
+            System.out.println("삭제 오류");
+            e.printStackTrace();
+        }
+    }
 }
